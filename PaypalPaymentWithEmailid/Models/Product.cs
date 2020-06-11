d6d63571e572aba40a7990567534a19fb80630e5
Nanhe Siddique <nanhesiddi58@gmail.com>
2020-06-11 13:45:38 +0530

    Add project files.

---
+++ PaypalPaymentWithEmailid/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PaypalPaymentWithEmailid.Models
+{
+    public class Product
+    {
+        [Required]
+        public string Price { get; set; }
+    }
+}
